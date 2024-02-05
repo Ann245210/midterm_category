0a7b91aff170e20e4ba8b9ae0b6f1cafb96ab7f2
Ann <forever970980@gmail.com>
2024-02-05 15:28:12 +0800

    修改sivebar樣式

--- primary_category.php
+++ primary_category.php
@@ -276,7 +276,7 @@
         <li class="nav-item">
           <a class="nav-link " href="../coupon_pages/coupon.php">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
+            <i class="fa-solid fa-ticket-simple text-dark text-sm opacity-10 fa-fw"></i>
             </div>
             <span class="nav-link-text ms-1">優惠券管理</span>
           </a>
